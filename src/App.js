e0fa878a6d893914da9fdc6f0404430373398e03
rahatkabir007 <rahatkabir04@gmail.com>
2023-01-16 00:32:37 +0600

    loader added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Works from './Works/Works';
 import './App.scss';
 import Home from './Home';
 import NotFound from './NotFound/NotFound';
+import Loader from './components/Loader/Loader';
 const App = () => {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false)
+        }, 4000)
+    }, [loading])
+
+    if (loading) return <Loader />;
+
     return (
         <div className='app'>
             <Router>
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Loader.css";
+const Loader = () => {
+    return (
+        <div className="loader-container">
+            <div class="loadingio-spinner-cube-m29t9w21eu"><div class="ldio-nv24sw08439">
+                <div></div><div></div><div></div><div></div>
+            </div></div>
+        </div>
+    );
+};
+
+export default Loader;
